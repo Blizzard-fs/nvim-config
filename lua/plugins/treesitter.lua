08f4358dae0ad4d4587088d00ae4d928649c1fae
Stefan <stefan.webbouw@gmail.com>
2024-03-19 22:37:06 +0100

    Add more ensure_installed languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()        
+    config = function()
         local builtin = require("telescope.builtin")
+        local config  = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {"lua", "javascript", "css", "php", "html", "json", "typescript"}
+        })
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
