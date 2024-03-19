bcf074031fd74384f86438458a4aa035d850bba2
Stefan <stefan.webbouw@gmail.com>
2024-03-19 09:45:06 +0100

    fix intelephense/html configs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,8 @@ return {
                     "lua_ls",
                     "intelephense",
                     "sqlls",
-                    "lemminx"
+                    "lemminx",
+                    "html"
                 }
             })
         end
@@ -34,10 +35,41 @@ return {
            lspconfig.docker_compose_language_service.setup({})
            lspconfig.jsonls.setup({})
            lspconfig.quick_lint_js.setup({})
-           lspconfig.lua_ls.setup({})
-           lspconfig.intelephense.setup({})
+           lspconfig.lua_ls.setup(
+           {
+                settings =
+                {
+                    Lua =
+                    {
+                        diagnostics =
+                        {
+                            global = { 'vim' }
+                        }
+                    }
+                }
+           })
+           lspconfig.intelephense.setup({
+                cmd = { "intelephense", "--stdio" },
+                filetypes = { "php" },
+                root_dir = lspconfig.util.root_pattern("", ".git")
+            })
            lspconfig.sqlls.setup({})
            lspconfig.lemminx.setup({})
+           lspconfig.html.setup({
+                capabilities = vim.lsp.protocol.make_client_capabilities(), -- Set client capabilities
+                cmd = { "vscode-html-language-server", "--stdio" }, -- Specify the command to start the language server
+                filetypes = { "html", "templ" }, -- Specify supported filetypes
+                init_options = { -- Specify init options
+                    configurationSection = { "html", "css", "javascript" },
+                    embeddedLanguages = {
+                        css = true,
+                        javascript = true
+                    },
+                    provideFormatter = true
+                },
+                root_dir = lspconfig.util.root_pattern("", ".git"), -- Specify root directory detection
+                settings = {}
+           })
         end
     }
 }
