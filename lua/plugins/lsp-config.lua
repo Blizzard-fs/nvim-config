d541962903f9b1347d4b42e76a05ac4c6240a76d
Stefan <stefan.webbouw@gmail.com>
2024-03-19 21:23:05 +0100

    Adding keymappings for LSPconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,8 @@ return {
                     "lua_ls",
                     "intelephense",
                     "sqlls",
-                    "lemminx"
+                    "lemminx",
+                    "html"
                 }
             })
         end
@@ -34,10 +35,49 @@ return {
            lspconfig.docker_compose_language_service.setup({})
            lspconfig.jsonls.setup({})
            lspconfig.quick_lint_js.setup({})
-           lspconfig.lua_ls.setup({})
-           lspconfig.intelephense.setup({})
+           lspconfig.lua_ls.setup(
+           {
+                settings =
+                {
+                    Lua =
+                    {
+                        diagnostics =
+                        {
+                            global = { 'vim' }
+                        }
+                    }
+                }
+           })
+           lspconfig.intelephense.setup({
+                cmd = { "intelephense", "--stdio" },
+                filetypes = { "php" },
+                root_dir = lspconfig.util.root_pattern("", ".git")
+            })
            lspconfig.sqlls.setup({})
            lspconfig.lemminx.setup({})
+           lspconfig.html.setup({
+                capabilities = vim.lsp.protocol.make_client_capabilities(),
+                cmd = { "vscode-html-language-server", "--stdio" },
+                filetypes = { "html", "templ" },
+                init_options = {
+                    configurationSection = { "html", "css", "javascript" },
+                    embeddedLanguages = {
+                        css = true,
+                        javascript = true
+                    },
+                    provideFormatter = true
+                },
+                root_dir = lspconfig.util.root_pattern("", ".git"),
+                settings = {}
+           })
+
+        -- Keybinds
+
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+        vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
