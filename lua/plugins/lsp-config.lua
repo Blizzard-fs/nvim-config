7096ce29f2c1b2104ef8cc88127fa9c87ff1a431
Stefan <stefan.webbouw@gmail.com>
2024-03-25 16:45:34 +0100

    Add php file extension to emmet language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -52,6 +52,7 @@ return {
                     "pug",
                     "typescriptreact",
                     "vue",
+                    "php",
                 },
                 init_options = {
                     html = {
