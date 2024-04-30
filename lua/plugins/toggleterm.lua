ac2aacb47800b0cbd696badd8dc9393209a327c9
Stefan <stefan.webbouw@gmail.com>
2024-04-30 21:32:04 +0200

    Added new plugin to toggle terminal

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,15 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function ()
+        require("toggleterm").setup{}
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {})
+        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], {})
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], {})
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], {})
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], {})
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], {})
+        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], {})
+		vim.keymap.set("n", "<C-t>", ":ToggleTerm<CR>", {})
+    end,
+}
