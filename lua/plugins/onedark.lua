b304d637b15c2f6f843ee60deb026ba052bedc60
Stefan <stefan.webbouw@gmail.com>
2024-04-30 21:41:54 +0200

    Add One Dark Theme configuration

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,15 @@
+return {
+    "navarasu/onedark.nvim",
+    config = function ()
+        require('onedark').setup {
+            style = 'darker',
+            transparent = true,
+            term_colors = true,
+            ending_tildes = true,
+            lualine = {
+                transparent = true,
+            }
+        }
+        require('onedark').load()
+    end,
+}
