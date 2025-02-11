de34075d8cf767f45cf936bc3cf703958edd7079
Blizzard-fs <stefan.webbouw@gmail.com>
2025-02-12 00:49:34 +0100

    idk

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,31 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local builtin = require("telescope.builtin")
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.6",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
 
-        vim.keymap.set("n", "<leader>dg", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("i", "jj", "<Esc>", {})
-    end,
+		vim.keymap.set("n", "<leader>dg", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("i", "jj", "<Esc>", {})
+
+		require("telescope").setup({
+			defaults = {
+				border = true,
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+			},
+		})
+	end,
 }, {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-        require("telescope").setup({
-            extensions = {
-                ["ui-select"] = {
-                    require("telescope.themes").get_dropdown({}),
-                },
-            },
-        })
-        require("telescope").load_extension("ui-select")
-    end,
+	"nvim-telescope/telescope-ui-select.nvim",
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
+				},
+			},
+		})
+		require("telescope").load_extension("ui-select")
+	end,
 }
